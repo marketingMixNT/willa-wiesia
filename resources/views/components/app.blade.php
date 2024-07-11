24f6709dc68982362acd474433175235c394562b
marekgacek45 <marekgacek45@gmail.com>
2024-07-11 12:33:20 +0200

    first commit

---
+++ resources/views/components/app.blade.php
@@ -0,0 +1,50 @@
+@props(['title', 'description', 'noFollow' => false])
+
+
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="application-name" content="{{ config('app.name') }}">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="keywords" content="">
+
+    {{-- No Follow --}}
+    @if ($noFollow)
+        <meta name="robots" content="noindex, nofollow">
+    @endisset
+
+    <!--Title-->
+    <title>{{ isset($title) ? $title : '' }} - Sunny Shoreline</title>
+    <meta name="description" content="{{ isset($description) ? $description : '' }}">
+
+
+    <!--Cannonical-->
+    <link rel="canonical" href="{{ url()->current() }}" />
+
+    <!--Favicon-->
+    {{-- @include('partials.favicon') --}}
+
+    <!--Fonts-->
+    {{-- @include('partials.fonts') --}}
+
+
+
+
+    @filamentStyles
+    @vite('resources/scss/app.scss')
+
+</head>
+
+
+
+@filamentScripts()
+@vite('resources/js/app.js')
+</body>
+
+</html>
+</body>
+
+</html>
---
+++ resources/views/pages/home/index.blade.php
@@ -0,0 +1,3 @@
+<x-layouts.app title="" description="">
+    
+</x-layouts.app>
